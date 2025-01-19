c782068524071e5ba15b365c3ffc06ef587d23ba
Gabriel Bury <gabrielbury@hotmail.com>
2025-01-19 11:31:04 -0300

    Initial commit

---
+++ src/main/java/com/dn325/batchconsumer/BatchConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.dn325.batchconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dn325/batchconsumer/BatchConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dn325.batchconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
